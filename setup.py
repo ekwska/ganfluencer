58d65580d8ca5c09b9aaf78bb835f76ff6248d9a
ekwiecinska <ekwiecinska55@gmail.com>
2020-08-13 11:05:34 +0100

    Add installation instructions
    Add tensorboard to requirements
    
    Fix setup.py README instructions  and remove outdated dependencies from setup.py
    
    Add example images to README of beauty influencer GAN
    
    Add instructions on how to run training and modify paths in config

--- setup.py
+++ setup.py
@@ -10,13 +10,6 @@ version = "0.0.1"
 with io.open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    "tensorflow >= 2.1.0",
-    "pytorch >= 1.0.2",
-    "numpy >= 1.18.1",
-    "matplotlib >= 3.1.2",
-]
-
 if sys.argv[-1] == "readme":
     print(readme)
     sys.exit()
@@ -31,13 +24,12 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Emma Kwiecinska",
-    author_email="ekwiecinska55@gmail.com",
-    url="https://github.com/ekwiecinska/ganfluencer",
+    author_email="ekwska0@protonmail.com",
+    url="https://github.com/ekwska/ganfluencer",
     packages=["ganfluencer",],
     package_dir={"ganfluencer": "ganfluencer"},
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=requirements,
     license="MIT",
     classifiers=[
         "Intended Audience :: Developers",
